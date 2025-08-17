fca2f1c76e2959dd8d73fa5af6571b09ab244502
mohammad-alz <mohammad.alz729@gmail.com>
2025-08-17 19:20:32 +0330

    fix .

--- api/views.py
+++ api/views.py
@@ -253,7 +253,7 @@ class PriceChartView(APIView):
         elif timeframe == 'monthly':
             start_date = end_date - timedelta(days=30)
         else: # 'daily'
-            start_date = end_date - timedelta(days=1)
+            start_date = end_date - timedelta(hours=24)
         
         # 3. Fetch all relevant data points from the database
         queryset = Price.objects.filter(
